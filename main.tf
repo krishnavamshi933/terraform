1297993908ed70baeb97993373383d8b3ecc1fc7
krishnavamshi933 <ckrishnavamshi595@gmail.com>
2023-03-25 12:48:57 +0530

    bv

--- main.tf
+++ main.tf
@@ -7,3 +7,44 @@ resource "aws_vpc" "example_vpc" {
   tags = {
     Name = "example-vpc"
   }
+}
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.example_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-2"
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.example_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-2"
+
+  tags = {
+    Name = "private-subnet"
+  }
+}
+resource "aws_internet_gateway" "example_igw" {
+  vpc_id = aws_vpc.example_vpc.id
+
+  tags = {
+    Name = "example-igw"
+  }
+}
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.example_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myigw.id
+  }
+}
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.example_vpc.id
+}
+resource "aws_route_table_association" "public_route_table_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
